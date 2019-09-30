d7d81664696be2976577db6e6a3052a2ea4554bd
Jesse <s216394139@mandela.ac.za>
2019-09-30 22:17:10 +0200

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookKeeper.Startup))]
+namespace BookKeeper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
