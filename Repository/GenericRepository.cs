87f0a852aa3628c8b9b9601f247d146d2a21c988
Mcbey, Jesse, (Mr) (s216394139) <s216394139@Mandela.ac.za>
2019-10-04 11:03:17 +0200

    NameSpace Issues Preventing The Creation Of Branch Tut 5+6? Trying to upload first to branchtut5 first...

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookKeeper.Repository
+{
+    public class GenericRepository<Tbl_Entity> : IRepository <Tbl_Entity> where Tbl_Entity:class
+    {
+        DbSet<> 
+    }
+}
---
+++ Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookKeeper.Repository
+{
+    public interface IRepository<Tbl_Entity> where Tbl_Entity:class
+    {
+        IEnumerable<Tbl_Entity> GetAllRecords();
+        IQueryable<Tbl_Entity> GetAllRecordsIQueryable();
+        int GetAllRecordsCount();
+        void Add(Tbl_Entity entity);
+        void Update(Tbl_Entity entity);
+        void UpdateByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict);
+        Tbl_Entity GetFirstorDefault(int recordId);
+        void Remove(Tbl_Entity entity);
+        void RemoveByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        void RemoveRangeByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        void InactiveAndDeleteMarkByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict);
+        Tbl_Entity GetFirstorDefaultByParameter(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        IEnumerable<Tbl_Entity> GetListParameter(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        IEnumerable<Tbl_Entity> GetResultBySqlProcedure(string query, params object[] parameters);
+    }
+}
